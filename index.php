3ed85804775895a3835381564074b8899859ec40
Jan Forman <info@janforman.com>
2019-11-27 09:27:15 +0100

    init

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+$url = 'https://'.$_SERVER['HTTP_HOST'].'/lineageos/';
+if($_GET['device'] == 'kingdom') {
+	$dir = 'kingdom';
+	$version = '16.0';
+}
+if($_GET['device'] == 'YTX703F') {
+	$dir = 'YTX703F';
+	$version = '16.0';
+}
+if($_GET['device'] == 'YTX703L') {
+	$dir = 'YTX703L';
+	$version = '16.0';
+}
+$files = scandir($dir,1);
+$file = $files[0];
+if ($version)
+{
+echo '{
+  "response": [
+    {
+      "datetime": ' . filemtime($dir . '/' . $file). ',
+      "filename": "' . $file . '",
+      "id": "' . md5($file). '",
+      "romtype": "UNOFFICIAL",
+      "size": ' . filesize($dir . '/' . $file). ',
+      "url": "'.$url.$dir.'/' . $file . '",
+      "version": "' . $version . '"
+    }
+  ]
+}
+';
+}
+else die('{"response": [{}]}');
