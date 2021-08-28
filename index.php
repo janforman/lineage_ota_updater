d4f600730a93f6f0c5c714e773bb48fab65b9d55
Jan Forman <48495095+janforman@users.noreply.github.com>
2021-08-28 14:33:11 +0200

    fix empty directory

--- index.php
+++ index.php
@@ -12,9 +12,9 @@ if($_GET['device'] == 'YTX703L') {
 	$dir = 'YTX703L';
 	$version = '16.0';
 }
-$files = scandir($dir,1);
+$files = array_diff(scandir($dir,1), array('..', '.'));
 $file = $files[0];
-if ($version)
+if ($version && $file)
 {
 echo '{
   "response": [
